9efcae193f023fe37de38f140baf7a5a9ae5df61
Osamu KOGA(osa_k) <osak.63@gmail.com>
2012-04-10 17:05:01 +0900

    POJのSample Input落としてファイルにしてくれるinitスクリプト追加

---
+++ init.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby
+
+require 'nokogiri'
+require 'open-uri'
+require 'fileutils'
+
+probno = ARGV[0].to_i
+
+doc = Nokogiri::HTML(open("http://poj.org/problem?id=#{probno}"))
+title = doc.css("div.ptt")[0].content
+
+filename = sprintf("%04d.cc", probno)
+if not FileTest.exist?(filename)
+  File.open(filename, 'w') do |f|
+    f.puts <<TEMPLATE
+//Name: #{title}
+//Level:
+//Category:
+//Note:
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+}
+TEMPLATE
+  end
+end
+
+sinput = doc.css("pre.sio")[0].content.gsub(/\r\n/, "\n")
+filename = sprintf("%04d.in", probno)
+File.open(filename, 'w') do |f|
+  f.puts(sinput)
+end
